9a866f2c68e5d70336fd8c0abf3ee711ec17cca9
amr8644 <72759681+amr8644@users.noreply.github.com>
2023-11-01 23:04:47 +0300

    New project

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-    tar := "https://www.w3schools.com/"
-    response :=SendRequests(tar)
-    ScrapeURL(*response,tar )
-    ParseResponse(*response)
+	tar := "https://www.shielder.com"
+	//  tar := "https://www.w3schools.com/"
+	//ParseResponse(*response)
+
+	links := LinkGrabber(tar)
+
+	for _, v := range links {
+		if v[0] == '/' {
+			fmt.Println(v)
+		}
+		if strings.Contains(v, tar) {
+			fmt.Println(v)
+			response := SendRequests(v)
+			ScrapeURL(*response, v)
+
+		}
+	}
 
 }
--- target.go
+++ target.go
@@ -20,8 +20,70 @@ type Data struct {
 
 var data Data
 
-func ParseHTMLPage(page string) {
+func PrintData() {
 
+	fmt.Println("URL:", data.URL)
+
+	//fmt.Println("HTML:", data.HTML)
+
+	fmt.Println("Headers:")
+	for k, v := range data.Headers {
+		if k == "X-Powered-By" {
+
+			fmt.Println("- ", k, ":", v)
+		}
+	}
+
+	fmt.Println("Cookies:")
+	for k, v := range data.Cookies {
+		fmt.Println("- ", k, ":", v)
+	}
+
+	fmt.Println("Meta:")
+	for k, v := range data.Meta {
+		fmt.Println("- ", k, ":", v)
+	}
+
+	fmt.Println("Script:")
+	for k, v := range data.Script {
+		fmt.Println("- ", k, ":", v)
+	}
+
+}
+
+func LinkGrabber(value string) []string {
+
+    var links []string
+		res := SendRequests(value)
+	body, err := ioutil.ReadAll(res.Body)
+
+	doc, err := html.Parse(strings.NewReader(string(body[:])))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+                    links = append(links, a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+    
+    return links
+}
+
+func ParseHTMLPage(page string) {
 	// Parse HTML
 	doc, err := html.Parse(strings.NewReader(data.HTML))
 
@@ -40,21 +102,18 @@ func ParseHTMLPage(page string) {
 			scripts = append(scripts, GetAttr(n, "src"))
 		}
 	})
-
 	// Print results
 	//fmt.Println("Meta:", meta)
 	//fmt.Println("Scripts:", scripts)
 }
 
 func ParseResponse(response http.Response) {
-
 	headers := response.Header
-
 	for k, v := range headers {
 		if k == "Set-Cookie" {
 			data.Cookies = make(map[string]string)
 			data.Cookies[k] = v[0]
-			fmt.Println(k, v)
+			//fmt.Println(k, v)
 		} else {
 			data.Headers = make(map[string][]string)
 			data.Headers[k] = v
@@ -76,7 +135,6 @@ func ParseRequest(response http.Response) {
 			data.Headers = make(map[string][]string)
 			data.Headers[k] = v
 		}
-
 	}
 }
 
@@ -90,7 +148,6 @@ func ScrapeURL(response http.Response, value string) {
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
-
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -100,6 +157,7 @@ func ScrapeURL(response http.Response, value string) {
 
 	for k, v := range headers {
 		data.Headers = make(map[string][]string)
+        fmt.Println(k,v)
 		data.Headers[k] = v
 	}
 
