7ad4c3915e1484368a20e1d4f29cef2749b3d3e1
amr8644 <amr8644@gmail.com>
2023-12-18 20:05:00 +0300

    Refactoring

--- main.go
+++ main.go
@@ -6,17 +6,10 @@ import (
 )
 
 func main() {
-	// Record the start time
+
 	startTime := time.Now()
 	Start()
-
-	// Record the end time
 	endTime := time.Now()
-
-	// Calculate the elapsed time
 	elapsedTime := endTime.Sub(startTime)
-
-	// Print the elapsed time
 	fmt.Println("Elapsed Time:", elapsedTime)
-	//PrintData()
 }
